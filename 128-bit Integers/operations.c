11dae715b6a0a7df5ee216fbcd3cde1d9c411e23
Alexander Elfimow <my.free.mail@gmx.de>
2018-10-03 21:56:04 +0200

    128-bit Integers: added file

---
+++ 128-bit Integers/operations.c
@@ -0,0 +1,20 @@
+
+__int128 add(__int128 a, __int128 b)
+{
+    return (a + b);
+}
+
+__int128 sub(__int128 a, __int128 b)
+{
+    return (a - b);
+}
+
+__int128 mul(__int128 a, __int128 b)
+{
+    return (a * b);
+}
+
+__int128 div(__int128 a, __int128 b)
+{
+    return (a / b);
+}
