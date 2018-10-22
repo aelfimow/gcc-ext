c65bfaa4fc2dfbb488ede951580f161a3f3d384a
Alexander Elfimow <my.free.mail@gmx.de>
2018-10-22 22:51:55 +0200

    Complex Numbers: set real and imag parts

--- Complex Numbers/main.c
+++ Complex Numbers/main.c
@@ -21,5 +21,9 @@ int main(int argc, char *argv[])
     print_complex(sum);
     print_complex(conj_sum);
 
+    __real__ a = 22.0;
+    __imag__ a = 33.0;
+    print_complex(a);
+
     return EXIT_SUCCESS;
 }
