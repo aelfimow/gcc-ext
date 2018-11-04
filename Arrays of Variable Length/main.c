0d3ac09d18331d1f41e0fa12871c11fdf5380c4b
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-04 22:31:45 +0100

    Arrays of Variable Length: added code

--- Arrays of Variable Length/main.c
+++ Arrays of Variable Length/main.c
@@ -1,7 +1,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+static void print_argv(int argc; char *argv[argc], int argc);
+
 int main(int argc, char *argv[])
 {
+    print_argv(argv, argc);
     return EXIT_SUCCESS;
+}
+
+static void print_argv(int argc; char *argv[argc], int argc)
+{
+    int i = 0;
+
+    while (i < argc)
+    {
+        printf("argv[%i] = %s\n", i, argv[i]);
+        ++i;
+    }
 }
