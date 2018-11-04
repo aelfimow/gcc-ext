65e54ef804f67c812e1fdbc5bb6b789f44e3539d
Alexander Elfimow <my.free.mail@gmx.de>
2018-11-04 21:42:12 +0100

    Structs with no members: added code

--- Structures with No Members/main.c
+++ Structures with No Members/main.c
@@ -1,7 +1,35 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+struct a
+{
+};
+
+struct b
+{
+    struct c
+    {
+    };
+};
+
+struct d
+{
+    struct e
+    {
+        struct f
+        {
+        };
+    };
+};
+
 int main(int argc, char *argv[])
 {
+    argc = argc;
+    argv = argv;
+
+    printf("sizeof a: %u\n", (unsigned int)sizeof(struct a));
+    printf("sizeof b: %u\n", (unsigned int)sizeof(struct b));
+    printf("sizeof d: %u\n", (unsigned int)sizeof(struct d));
+
     return EXIT_SUCCESS;
 }
